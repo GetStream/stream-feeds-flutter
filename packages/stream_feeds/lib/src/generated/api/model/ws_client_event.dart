86df5c5343c0e88c6ef9ad624d3c37fa1b5a2f7b
Rene Floor <rene.floor@getstream.io>
2025-08-06 15:38:06 +0200

    Add discriminator files to codecov ignore

--- packages/stream_feeds/lib/src/generated/api/model/ws_client_event.dart
+++ packages/stream_feeds/lib/src/generated/api/model/ws_client_event.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: public_member_api_docs, use_string_in_part_of_directives, prefer_single_quotes, require_trailing_commas
 
+// coverage:ignore-file
+
 import '../models.dart';
 
 sealed class WSClientEvent {
