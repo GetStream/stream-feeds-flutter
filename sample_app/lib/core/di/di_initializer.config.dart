af43bc6e60645f3859e94470555c46cd995e942d
Sahil Kumar <sahil@getstream.io>
2025-09-09 13:21:51 +0200

    chore(samples): rename sample app to sample_app and update import paths (#23)

--- sample_app/lib/core/di/di_initializer.config.dart
+++ sample_app/lib/core/di/di_initializer.config.dart
@@ -11,16 +11,17 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
-import 'package:sample/app/app_state.dart' as _i870;
-import 'package:sample/app/content/auth_controller.dart' as _i27;
-import 'package:sample/core/di/di_module.dart' as _i238;
-import 'package:sample/core/di/session_module.dart' as _i514;
-import 'package:sample/core/models/user_credentials.dart' as _i845;
-import 'package:sample/navigation/app_router.dart' as _i701;
-import 'package:sample/navigation/guards/auth_guard.dart' as _i1031;
-import 'package:sample/services/app_preferences.dart' as _i354;
 import 'package:shared_preferences/shared_preferences.dart' as _i460;
 import 'package:stream_feeds/stream_feeds.dart' as _i250;
+
+import '../../app/app_state.dart' as _i804;
+import '../../app/content/auth_controller.dart' as _i817;
+import '../../navigation/app_router.dart' as _i783;
+import '../../navigation/guards/auth_guard.dart' as _i253;
+import '../../services/app_preferences.dart' as _i825;
+import '../models/user_credentials.dart' as _i18;
+import 'di_module.dart' as _i211;
+import 'session_module.dart' as _i849;
 
 extension GetItInjectableX on _i174.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -38,20 +39,20 @@ extension GetItInjectableX on _i174.GetIt {
       () => appModule.prefs,
       preResolve: true,
     );
-    gh.singleton<_i870.AppStateNotifier>(() => _i870.AppStateNotifier());
-    gh.singleton<_i354.AppPreferences>(
-        () => _i354.AppPreferences(gh<_i460.SharedPreferences>()));
-    gh.lazySingleton<_i27.AuthController>(
-        () => _i27.AuthController(gh<_i354.AppPreferences>()));
-    gh.factory<_i1031.AuthGuard>(
-        () => _i1031.AuthGuard(gh<_i27.AuthController>()));
-    gh.factoryParam<_i250.StreamFeedsClient, _i845.UserCredentials, dynamic>((
+    gh.singleton<_i804.AppStateNotifier>(() => _i804.AppStateNotifier());
+    gh.singleton<_i825.AppPreferences>(
+        () => _i825.AppPreferences(gh<_i460.SharedPreferences>()));
+    gh.lazySingleton<_i817.AuthController>(
+        () => _i817.AuthController(gh<_i825.AppPreferences>()));
+    gh.factoryParam<_i250.StreamFeedsClient, _i18.UserCredentials, dynamic>((
       credentials,
       _,
     ) =>
         appModule.feed(credentials));
-    gh.lazySingleton<_i701.AppRouter>(
-        () => _i701.AppRouter(gh<_i1031.AuthGuard>()));
+    gh.factory<_i253.AuthGuard>(
+        () => _i253.AuthGuard(gh<_i817.AuthController>()));
+    gh.lazySingleton<_i783.AppRouter>(
+        () => _i783.AppRouter(gh<_i253.AuthGuard>()));
     return this;
   }
 
@@ -63,12 +64,12 @@ extension GetItInjectableX on _i174.GetIt {
       init: (_i526.GetItHelper gh) {
         final sessionModule = _$SessionModule();
         gh.singleton<_i250.StreamFeedsClient>(() =>
-            sessionModule.authenticatedFeedsClient(gh<_i27.AuthController>()));
+            sessionModule.authenticatedFeedsClient(gh<_i817.AuthController>()));
       },
     );
   }
 }
 
-class _$AppModule extends _i238.AppModule {}
+class _$AppModule extends _i211.AppModule {}
 
-class _$SessionModule extends _i514.SessionModule {}
+class _$SessionModule extends _i849.SessionModule {}
