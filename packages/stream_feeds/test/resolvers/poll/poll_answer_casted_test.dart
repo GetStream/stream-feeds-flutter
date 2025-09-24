158f43283fbed81c4c574ae43abd3773eed51d15
Rene Floor <rene.floor@getstream.io>
2025-09-24 14:04:57 +0200

    Add test for PollVoteChangedFeedEvent

--- packages/stream_feeds/test/resolvers/poll/poll_answer_casted_test.dart
+++ packages/stream_feeds/test/resolvers/poll/poll_answer_casted_test.dart
@@ -5,7 +5,7 @@ import 'package:stream_feeds/stream_feeds.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('pollAnswerCastedFeedEventResolver', () {
+  group('pollAnswerCastedFeedEventResolver PollVoteCastedFeedEvent', () {
     test('resolves Answer when answer is true', () {
       final event = createPollVoteCastedFeedEvent(isAnswer: true);
       final resolvedEvent = pollAnswerCastedFeedEventResolver(event);
@@ -19,6 +19,23 @@ void main() {
     });
     test('does not resolve Answer when answer is null', () {
       final event = createPollVoteCastedFeedEvent(isAnswer: null);
+      final resolvedEvent = pollAnswerCastedFeedEventResolver(event);
+      expect(resolvedEvent, isNull);
+    });
+  });
+  group('pollAnswerCastedFeedEventResolver PollVoteChangedFeedEvent', () {
+    test('resolves Answer when answer is true', () {
+      final event = createPollVoteChangedFeedEvent(isAnswer: true);
+      final resolvedEvent = pollAnswerCastedFeedEventResolver(event);
+      expect(resolvedEvent, isA<PollAnswerCastedFeedEvent>());
+    });
+    test('does not resolve Answer when answer is false', () {
+      final event = createPollVoteChangedFeedEvent(isAnswer: false);
+      final resolvedEvent = pollAnswerCastedFeedEventResolver(event);
+      expect(resolvedEvent, isNull);
+    });
+    test('does not resolve Answer when answer is null', () {
+      final event = createPollVoteChangedFeedEvent(isAnswer: null);
       final resolvedEvent = pollAnswerCastedFeedEventResolver(event);
       expect(resolvedEvent, isNull);
     });
@@ -60,5 +77,43 @@ PollVoteCastedFeedEvent createPollVoteCastedFeedEvent({bool? isAnswer}) {
       pollId: '1',
       isAnswer: isAnswer,
     ),
+  );
+}
+
+PollVoteChangedFeedEvent createPollVoteChangedFeedEvent({bool? isAnswer}) {
+  return PollVoteChangedFeedEvent(
+    createdAt: DateTime.now(),
+    custom: const {},
+    fid: '1',
+    poll: PollResponseData(
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+      id: '1',
+      allowAnswers: true,
+      allowUserSuggestedOptions: true,
+      answersCount: 1,
+      createdById: '1',
+      custom: const {},
+      description: '1',
+      enforceUniqueVote: true,
+      latestAnswers: const [],
+      latestVotesByOption: const {},
+      maxVotesAllowed: 1,
+      name: '1',
+      options: const [],
+      ownVotes: const [],
+      voteCount: 1,
+      voteCountsByOption: const {},
+      votingVisibility: '1',
+    ),
+    pollVote: PollVoteResponseData(
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+      id: '1',
+      optionId: '1',
+      pollId: '1',
+      isAnswer: isAnswer,
+    ),
+    type: 'poll.vote.changed',
   );
 }
