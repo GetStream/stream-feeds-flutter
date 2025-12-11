100867d440eb653758047c304e564ffb8f6e933b
Sahil Kumar <sahil@getstream.io>
2025-12-11 12:07:16 +0100

    feat: hide `defaultOnNewActivity` from state export

--- packages/stream_feeds/lib/stream_feeds.dart
+++ packages/stream_feeds/lib/stream_feeds.dart
@@ -3,4 +3,4 @@ export 'package:stream_core/stream_core.dart';
 export 'src/feeds_client.dart';
 export 'src/generated/api/api.dart' hide User;
 export 'src/models.dart';
-export 'src/state.dart';
+export 'src/state.dart' hide defaultOnNewActivity;
