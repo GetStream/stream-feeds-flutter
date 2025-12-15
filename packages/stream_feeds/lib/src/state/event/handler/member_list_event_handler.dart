318a364aa791751c3046531c6ec99fdbef0d08bc
Sahil Kumar <sahil@getstream.io>
2025-12-15 16:10:33 +0100

    feat: handle missing events in MemberList

--- packages/stream_feeds/lib/src/state/event/handler/member_list_event_handler.dart
+++ packages/stream_feeds/lib/src/state/event/handler/member_list_event_handler.dart
@@ -2,6 +2,7 @@ import 'package:stream_core/stream_core.dart';
 
 import '../../../generated/api/models.dart' as api;
 import '../../../models/feed_member_data.dart';
+import '../../../utils/filter.dart';
 import '../../member_list_state.dart';
 
 import '../../query/members_query.dart';
@@ -20,27 +21,30 @@ class MemberListEventHandler implements StateEventHandler {
   void handleEvent(WsEvent event) {
     final fid = query.fid;
 
-    bool matchesQueryFilter(FeedMemberData member) {
-      final filter = query.filter;
-      if (filter == null) return true;
-      return filter.matches(member);
-    }
-
-    if (event is api.FeedMemberRemovedEvent) {
+    if (event is api.FeedMemberAddedEvent) {
       if (event.fid != fid.rawValue) return;
-      return state.onMemberRemoved(event.memberId);
+      final member = event.member.toModel();
+      // Check if the new member matches the query filter
+      if (!member.matches(query.filter)) return;
+
+      return state.onMemberAdded(member);
     }
 
     if (event is api.FeedMemberUpdatedEvent) {
       if (event.fid != fid.rawValue) return;
 
       final member = event.member.toModel();
-      if (!matchesQueryFilter(member)) {
+      if (!member.matches(query.filter)) {
         // If the updated member no longer matches the filter, remove it
         return state.onMemberRemoved(member.id);
       }
 
       return state.onMemberUpdated(member);
+    }
+
+    if (event is api.FeedMemberRemovedEvent) {
+      if (event.fid != fid.rawValue) return;
+      return state.onMemberRemoved(event.memberId);
     }
 
     // Handle other events if needed
